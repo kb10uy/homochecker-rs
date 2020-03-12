e39839423e14c794efb37775ec679f2bdb17da80
Yū Kobayashi <kb10uy@kb10uy.org>
2020-03-12 20:06:32 +0900

    repository を書いた

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,57 @@ mod api;
 mod homo;
 mod repository;
 
+use std::{collections::HashMap, env::vars, net::SocketAddr, process::exit};
+
+use dotenv::dotenv;
+use log::{error, info};
+use r2d2::Pool;
+use r2d2_postgres::PostgresConnectionManager;
 use tokio;
+use tokio_postgres::NoTls;
 use warp::Filter;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    dotenv().ok();
+    pretty_env_logger::init();
+    let envs: HashMap<_, _> = vars().collect();
+
+    // コネクションプール
+    let db_config = envs
+        .get("DATABASE_CONFIG")
+        .unwrap_or_else(|| {
+            error!("Environment variable `DATABASE_CONFIG` must be set!");
+            exit(1);
+        })
+        .parse()
+        .unwrap_or_else(|e| {
+            error!("Failed to parse `DATABASE_CONFIG`: {}", e);
+            exit(1);
+        });
+
+    let manager = PostgresConnectionManager::new(db_config, NoTls);
+    let pool = Pool::new(manager).unwrap_or_else(|e| {
+        error!("Failed to create database connection pool: {}", e);
+        exit(1);
+    });
+
+    // サーバー
+    let listen_address: SocketAddr = envs
+        .get("LISTEN_ADDRESS")
+        .unwrap_or_else(|| {
+            error!("Environment variable `LISTEN_ADDRESS` must be set!");
+            exit(1);
+        })
+        .parse()
+        .unwrap_or_else(|e| {
+            error!("Failed to parse `LISTEN_ADDRESS`: {}", e);
+            exit(1);
+        });
+
     let routes = warp::any().and_then(action::check);
-    warp::serve(routes).run(([127, 0, 0, 1], 8000)).await;
+
+    info!("Listening on {}", listen_address);
+    warp::serve(routes).run(listen_address).await;
     Ok(())
 }
--- src/repository.rs
+++ src/repository.rs
@@ -1,2 +1,47 @@
-use tokio_postgres;
+//! Contains data repository.
 
+use std::error::Error;
+
+use tokio_postgres::{Client, Error as PostgresError, Row};
+
+/// It can be constructed from a row.
+pub trait FromRow
+where
+    Self: Sized,
+{
+    fn from_row(row: &Row) -> Result<Self, PostgresError>;
+}
+
+/// `users`
+pub struct User {
+    pub id: i32,
+    pub screen_name: String,
+    pub avatar_url: String,
+    pub service_url: String,
+}
+
+impl User {
+    /// Fetches all record from `users`.
+    pub async fn fetch_all<T>(client: &Client) -> Result<Vec<User>, Box<dyn Error>> {
+        let rows = client
+            .query(r#"SELECT * FROM "users" ORDER BY "id";"#, &[])
+            .await?;
+
+        rows.iter().try_fold(vec![], |mut users, row| {
+            users.push(User::from_row(row)?);
+            Ok(users)
+        })
+    }
+}
+
+// TODO: proc_macro 化の余地あり
+impl FromRow for User {
+    fn from_row(row: &Row) -> Result<Self, PostgresError> {
+        Ok(User {
+            id: row.try_get("id")?,
+            screen_name: row.try_get("screen_name")?,
+            avatar_url: row.try_get("avatar_url")?,
+            service_url: row.try_get("service_url")?,
+        })
+    }
+}
